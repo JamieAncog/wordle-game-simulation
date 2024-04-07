225763afdb08f66d68875cc275e03883514a0a89
Jamie Ancog <jamieancog@gmail.com>
2024-04-07 00:49:09 -0700

    finished almost final draft

--- wordle.cpp
+++ wordle.cpp
@@ -14,8 +14,9 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-void findPossibilities(const string& in, int n, std::string& floating, vector<string> posb, vector<string>& final, const std::set<std::string>& dict);
-int spacesLeft(string floatStr);
+void findPossibilities(string floatsLeft, const string& in, int n, std::string& floating, vector<string> posb, std::set<std::string>& final, const std::set<std::string>& dict);
+int spacesLeft(string inp, int curr);
+void nextLetter(string prefix, string floatsLeft, const string& in, int n, std::string& floating, vector<string> posb, std::set<std::string>& final, const std::set<std::string>& dict);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -26,26 +27,26 @@ std::set<std::string> wordle(
     // Add your code here
     //Find Possibilities
     vector<string> posb;
-    vector<string> final;
-    string floats = floating;
-    findPossibilities(in, 0, floats, posb, final, dict);
-    //Test if word and convert to set
     std::set<std::string> mySet;
-    for (int i = 0; i < (int) final.size(); i++){
-        mySet.insert(final[i]);
+    string floaters = floating;
+    string floats = "";
+    for (int i = 0; i < (int) in.length(); i++){
+        if (in.substr(i,1).compare("-") == 0){
+            floats += "-";
+        }
     }
+    findPossibilities(floats, in, 0, floaters, posb, mySet, dict);
     return mySet;
 }
 
 // Define any helper functions 
-
-void findPossibilities(const string& in, int n, std::string& floating, vector<string> posb, vector<string>& final, const std::set<std::string>& dict){
+void findPossibilities(string floatsLeft, const string& in, int n, std::string& floating, vector<string> posb, std::set<std::string>& final, const std::set<std::string>& dict){
     if (n == (int) in.length()) {
         for (int i = 0; i < (int) posb.size(); i++){
             std::set<std::string>::iterator it;
             it = dict.find(posb[i]);
             if (it != dict.end()){
-                final.push_back(posb[i]);
+                final.insert(posb[i]);
             }
         }
     }
@@ -61,51 +62,35 @@ void findPossibilities(const string& in, int n, std::string& floating, vector<st
                     nextPos.push_back(posb[i] + in.substr(n,1));
                 }
             }
-            findPossibilities(in, n+1, floating, nextPos, final, dict);
+            findPossibilities(floatsLeft, in, n+1, floating, nextPos, final, dict);
         }
         //Case 2: Space is not green
         else {
-            string alph = "abcdefghijklmnopqrstuvwzyz";
             for (int k = 0; k < (int) posb.size(); k++){
-                for (int i = 0; i < (int) floating.size(); i++){
-                    string nextStr = posb[k] + floating.substr(i,1);
-                    string newfloating = floating.substr(0,i) + floating.substr(i+1);
-                    nextPos.push_back(nextStr);
-                    findPossibilities(in, n+1, newfloating, nextPos, final, dict);
-                }
-                if ((int) floating.length() < spacesLeft(floating) || floating.length() == 0){
-                    for (int j = 0; j < 26; j++){
-                        string nextStr = posb[k] + alph.substr(j,1);
-                        nextPos.push_back(nextStr);
-                    }
-                    findPossibilities(in, n+1, floating, nextPos, final, dict);
-                }
+                nextLetter(posb[k], floatsLeft, in, n, floating, posb, final, dict);
             }
             if (posb.size() == 0){
-                for (int i = 0; i < (int) floating.size(); i++){
-                    string nextStr = floating.substr(i,1);
-                    string newfloating = floating.substr(0,i) + floating.substr(i+1);
-                    nextPos.push_back(nextStr);
-                    findPossibilities(in, n+1, newfloating, nextPos, final, dict);
-                }
-                if ((int) floating.length() < spacesLeft(floating) || floating.length() == 0){
-                    for (int j = 0; j < 26; j++){
-                        string nextStr = alph.substr(j,1);
-                        nextPos.push_back(nextStr);
-                    }
-                    findPossibilities(in, n+1, floating, nextPos, final, dict);
-                }
+                nextLetter("", floatsLeft, in, n, floating, posb, final, dict);
             }
         }
     }
 }
 
-int spacesLeft(string floatStr){
-    int count = 0;
-    for (int i = 0; i < (int) floatStr.length(); i++){
-        if (floatStr.substr(i,1).compare("-") == 0){
-            count++;
-        }
+void nextLetter(string prefix, string floatsLeft, const string& in, int n, std::string& floating, vector<string> posb, std::set<std::string>& final, const std::set<std::string>& dict){
+    string alph = "abcdefghijklmnopqrstuvwzyz";
+    vector<string> nextPos;
+    for (int i = 0; i < (int) floating.size(); i++){
+        string nextStr = prefix + floating.substr(i,1);
+        string newfloating = floating.substr(0,i) + floating.substr(i+1);
+        nextPos.push_back(nextStr);
+        int len = floatsLeft.length();
+        findPossibilities(floatsLeft.substr(0,len-1),in, n+1, newfloating, nextPos, final, dict);
     }
-    return count;
+    if (floatsLeft.length() < in.length() - prefix.length() || floating.length() == 0){
+        for (int j = 0; j < 26; j++){
+            string nextStr = prefix + alph.substr(j,1);
+            nextPos.push_back(nextStr);
+        }
+        findPossibilities(floatsLeft, in, n+1, floating, nextPos, final, dict);
+    }
 }
