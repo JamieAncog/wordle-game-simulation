da5643491efdca619c39f8543931a9ad9c539311
Jamie Ancog <jamieancog@gmail.com>
2024-04-14 16:49:58 -0700

    fixed wordle helper

--- wordle.cpp
+++ wordle.cpp
@@ -38,11 +38,10 @@ std::set<std::string> wordle(
 
 // Define any helper functions 
 void findPossibilities(int blanks, int floatsLeft, const string& in, int n, std::string& floating, vector<string> posb, std::set<std::string>& final, const std::set<std::string>& dict){
-    /*for (int i = 0; i < (int) posb.size(); i++){
+    for (int i = 0; i < (int) posb.size(); i++){
         cout << posb[i] << " ";
     }
     cout << endl;
-    */
     if (n == (int) in.length()) {
         for (int i = 0; i < (int) posb.size(); i++){
             std::set<std::string>::iterator it;
